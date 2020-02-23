6526e1e244af5c0f002334068ed467b3d6ff912d
bnates <runner@fv-az32.2r1r2hekggaejmsyeeanc2tgtg.cx.internal.cloudapp.net>
2020-02-23 15:04:48 +0000

    Updates

---
+++ precache-manifest.0923c45ed4868a4baed0d38a481bb93d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d00ee8ace696a416e08854f4d0ef753",
+    "url": "/cool-react/index.html"
+  },
+  {
+    "revision": "b523aa0a4733b715e12d",
+    "url": "/cool-react/static/css/main.479bcff0.chunk.css"
+  },
+  {
+    "revision": "db46f534a6e0eabf8308",
+    "url": "/cool-react/static/js/2.9a427b1e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cool-react/static/js/2.9a427b1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b523aa0a4733b715e12d",
+    "url": "/cool-react/static/js/main.69840c1d.chunk.js"
+  },
+  {
+    "revision": "c8bfad056916a4654281",
+    "url": "/cool-react/static/js/runtime-main.bbed8e9d.js"
+  }
+]);
